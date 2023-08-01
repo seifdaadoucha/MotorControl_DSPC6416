6065ff2549ee554d54652111c85387f5fc472209
seifdaadoucha <seif.daadoucha@gmail.com>
2023-08-01 18:28:24 +0100

    Updated code for motor control

--- main.c
+++ main.c
@@ -1,33 +1,29 @@
 #include <stdio.h>
 #include <unistd.h>
 
-// Hypothetical GPIO library functions
-void gpio_setup(int pin_number, int direction);
-void gpio_set(int pin_number, int value);
-void gpio_cleanup(int pin_number);
-
-// Define GPIO pin number for motor control
-#define MOTOR_PIN  10
+// Function to set the GPIO pin direction and value
+void set_gpio_pin(int pin_number, int direction, int value) {
+    // Add your implementation here using the actual GPIO functions provided by your DSPC6416 platform
+    // For example, using CSL GPIO functions on the TMS320C6416 DSK
+}
 
 int main() {
-    // Initialize the GPIO pin for motor control
-    gpio_setup(MOTOR_PIN, 1); // Assuming 1 for output direction
+    // Initialize the GPIO pin for motor control as an output
+    set_gpio_pin(0, GPIO_OUTPUT, 0); // Set the GPIO pin low initially
 
     while (1) {
         // Turn ON the motor for 2 seconds
-        gpio_set(MOTOR_PIN, 1); // Set the GPIO pin high to activate the motor
+        set_gpio_pin(0, GPIO_OUTPUT, 1); // Set the GPIO pin high to activate the motor
 
         printf("Motor is ON\n");
         sleep(2);
 
         // Turn OFF the motor for 2 seconds
-        gpio_set(MOTOR_PIN, 0); // Set the GPIO pin low to deactivate the motor
+        set_gpio_pin(0, GPIO_OUTPUT, 0); // Set the GPIO pin low to deactivate the motor
 
         printf("Motor is OFF\n");
         sleep(2);
     }
 
-    // Cleanup GPIO and release any resources
-    gpio_cleanup(MOTOR_PIN);
     return 0;
 }
